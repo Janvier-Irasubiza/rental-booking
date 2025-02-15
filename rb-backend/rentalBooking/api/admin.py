1e7071f10fae9d0a340db9ced3b491a0501ddbd7
Janvier-Irasubiza <61458240+Janvier-Irasubiza@users.noreply.github.com>
2025-02-15 22:11:49 +0200

    Implement custom authentication forms and email-based login for user management

--- rb-backend/rentalBooking/api/admin.py
+++ rb-backend/rentalBooking/api/admin.py
@@ -1,21 +1,33 @@
-# backend/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .forms import AuthenticationForm
 from .models import User
+from .forms import CustomUserCreationForm, CustomUserChangeForm, EmailAuthenticationForm
 
 class CustomUserAdmin(UserAdmin):
-    form = AuthenticationForm
-    add_fieldsets = (
-        (None, {
-            'fields': ('email', 'password1', 'password2'),
-        }),
-    )
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone_number')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
 
-# Register the custom User model with the custom UserAdmin
+# Register admin class
 admin.site.register(User, CustomUserAdmin)
-admin.site.login_form = AuthenticationForm
-admin.site.login_template = 'admin/login.html'
+
+# Override admin login form
+admin.site.login_form = EmailAuthenticationForm
